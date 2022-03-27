3ecb5124d9ca554dd92e3417d0e295b8e9c04176
Ibikunle Jamiu <98085958+thealphameil@users.noreply.github.com>
2022-03-27 19:01:39 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const { tours } = require('./API/tours')
+
+app.get('/api/tours', (req, res) => {
+  res.status(200).json(tours)
+})
+
+app.get('/api/tours/:tourID', (req, res) => {
+  const { tourID } = req.params
+  const singleTour = tours.find((tour) => tour.id === Number(tourID))
+  if(!singleTour) res.send('No match found.')
+  res.json(singleTour)
+})
+
+app.listen(8080, () => {
+  console.log('Server is listening at port 8080.')
+})
